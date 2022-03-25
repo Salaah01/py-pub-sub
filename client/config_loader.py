478c9dc86fffc62449444ccd8bd76199a529d8f8
Salaah Amin <salaah_amin@hotmail.co.uk>
2022-03-25 12:16:55 +0000

    replaced the symbolic link with the actual file contents to support windows.

--- client/config_loader.py
+++ client/config_loader.py
@@ -1,1 +1,9 @@
-../server/config_loader.py
+"""A helper module for loading the config from the root."""
+import sys
+import os
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+)
+import config  # noqa: E402, F401
